627e7abb84f26de6335233996b829affab4c61d8
alekseizdornikov <138918920+alekseizdornikov@users.noreply.github.com>
2023-07-10 20:53:56 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -4,10 +4,3 @@ def str_upper(string: str):
     """
     return string.upper()
 
-def str_title(string: str):
-    """
-    Функция выводит строку с заглавными буквами в словах
-    """
-    str_list = string.split(' ')
-    str_list = [word.title() for word in str_list]
-    return ' '.join(str_list)
